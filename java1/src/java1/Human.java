e270311a03bd1711bf592ae40d7e63b84311ef19
ADMIN <ADMIN@DESKTOP-VIHEGG3>
2024-12-31 10:41:20 +0530

    New code

---
+++ java1/src/java1/Human.java
@@ -0,0 +1,9 @@
+package java1;
+
+public class Human {
+	String name;
+	Human(){
+		this.name="ganga";
+	}
+
+}
---
+++ java1/src/java1/Student.java
@@ -0,0 +1,13 @@
+package java1;
+
+public class Student extends Human {
+	int id=10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student();
+		System.out.print(s.name+" "+s.id);
+
+	}
+
+}
